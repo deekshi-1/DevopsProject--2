2e51cf71eab85786644c6deca28eed78d98d6a7e
deekshi-1 <deekshithc1@gmail.com>
2026-07-30 15:10:32 +0530

    updated backend

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,10 @@
 terraform {
-    backend "s3" {
-        bucket = "capstone-deekshith-tfstate-bucket"
-        key    = "dev/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform-locks"
-        encrypt = true
-    }   
+  backend "s3" {
+    bucket         = "capstone-deekshith-tfstate-bucket"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+    use_lockfile   = true
+  }
 }

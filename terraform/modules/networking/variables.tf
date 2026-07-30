6babab79854062b8375874cc799171ff7cbdc154
deekshi-1 <deekshithc1@gmail.com>
2026-07-30 12:42:49 +0530

    cleaner

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,15 +1,5 @@
-variable "vpc_cidr" {
-    type = string  
-}
-variable "public_subnet_cidr" {
-    type = list(string) 
-}
-variable "private_subnet_cidr" {
-    type = list(string) 
-}
-variable "availability_zone" {
-    type = list(string) 
-}
-variable "environment" {
-    type = string  
-}
+variable "vpc_cidr"            { type = string }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs"{ type = list(string) }
+variable "azs"                 { type = list(string) }
+variable "env"                 { type = string }

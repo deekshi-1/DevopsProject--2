af93cfe32de77c5db779554a5d05fd7c2e5a5dc8
deekshi-1 <deekshithc1@gmail.com>
2026-07-30 10:39:40 +0530

    base app

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(status="healthy", hostname=socket.gethostname())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
